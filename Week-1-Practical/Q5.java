574f6ad37bdada9b598f0f30d860f976306cb4e3
Bhumik Sharma <bhsharma2007@gmail.com>
2026-08-01 22:58:30 +0530

    Add files via upload

---
+++ Week-1-Practical/Q5.java
@@ -0,0 +1,12 @@
+//Leetcode Question 2235. Add Two Integers
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1+num2;
+    }
+}
+public class Q5{
+    public static void main(String[] args){
+    Solution s = new Solution();
+    int result = s.sum(4,5);
+    System.out.print(result);
+}}
